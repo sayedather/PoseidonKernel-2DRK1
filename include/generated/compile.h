6be4e474e60571af949d9b01757a8f8ad32544a2
Atherrepo <ather90@gmail.com>
2019-04-07 14:47:03 -0700

    Source Upload

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Sun Apr 7 14:41:45 PDT 2019"
+#define LINUX_COMPILE_BY "sayedather"
+#define LINUX_COMPILE_HOST "triplr-dev.server"
+#define LINUX_COMPILER "gcc version 4.8 (GCC) "
